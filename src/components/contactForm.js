43b910ed10b440805aec79d5de542866e8cfc87a
AngeloJay <AngeloJay@users.noreply.github.com>
2024-05-16 21:53:20 -0700

    More stylization added with contact form
    Contact form added

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react'
 import './contactForm.css';
+import emailjs from '@emailjs/browser';
 
-const contactForm = () => {
+const ContactForm = () => {
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('x', 'x', form.current, {
+        publicKey: 'x',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert('Email has been sent!')
+          form.current.reset();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          alert('Sorry something went wrong. Please reach out another way if the problemss')
+        },
+      );
+  };
+
   return (
     <div className='contact-div'>
+    <form ref={form} onSubmit={sendEmail}>
+        <p>Name:</p>
+        <input  className='email' type="text" name="user_name" required/>
         <p>E-mail:</p>
-        <input className='toAndFrom' type='email' required/>
-
+        <input className='email' type="email" name="user_email" required/>
         <p>Message:</p>
-        <input className='msg' type='text' required/>
-
-        <button>Submit</button>
+        <textarea className='msg' required/>
+        <input className='submit-form' type="submit" value="Send" />
+    </form>
     </div>
   );
 };
 
-export default contactForm;
+export default ContactForm;
